45843af4d6072ef1dddefc6380326247f900759f
mauraaninditakirana <kiranamaura351@gmail.com>
2026-03-02 09:56:25 +0700

    menambahkan dependency pada file user

--- src/main/java/com/deploy/praktikum1/model/entity/User.java
+++ src/main/java/com/deploy/praktikum1/model/entity/User.java
@@ -1,4 +1,18 @@
 package com.deploy.praktikum1.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name ="users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
 }
